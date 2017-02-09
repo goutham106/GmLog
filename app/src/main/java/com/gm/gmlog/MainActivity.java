e8208a73a53e1827b7a9b194d071a994a9b75cb0
Goutham <goutham.gm11@gmail.com>
2017-02-09 18:01:32 +0530

    Test Commit1

--- app/src/main/java/com/gm/gmlog/MainActivity.java
+++ app/src/main/java/com/gm/gmlog/MainActivity.java
@@ -2,6 +2,7 @@ package com.gm.gmlog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: TEst");
     }
 }
